0e0d0df4363d32e10de836bf4fb90795a86508c0
Adam Nickle <anickle060193@gmail.com>
2018-03-04 19:59:22 -0600

    Make tabs draggable.

--- src/components/DocumentTabs/index.tsx
+++ src/components/DocumentTabs/index.tsx
@@ -5,7 +5,9 @@ import './styles.css';
 
 interface State
 {
+  tabs: string[];
   activeTab: number;
+  dragging: number | null;
 }
 
 class DocumentTabs extends React.Component<{}, State>
@@ -15,19 +17,19 @@ class DocumentTabs extends React.Component<{}, State>
     super( props );
 
     this.state = {
-      activeTab: 0
+      tabs: [ 'Tab 1', 'Tab 2', 'Tab 3', 'Tab 4', 'Tab 5Tab 5Tab 5Tab 5Tab 5Tab 5' ],
+      activeTab: 0,
+      dragging: null
     };
   }
 
   render()
   {
-    let tabs = [ 'Tab 1', 'Tab 2', 'Tab 3', 'Tab 4', 'Tab 5Tab 5Tab 5Tab 5Tab 5Tab 5' ];
-
-    let tabCount = tabs.length;
+    let tabCount = this.state.tabs.length;
 
     return (
       <ul className="document-tabs">
-        {tabs.map( ( tab, i ) => (
+        {this.state.tabs.map( ( tab, i ) => (
           <li
             key={i}
             className={[
@@ -37,7 +39,12 @@ class DocumentTabs extends React.Component<{}, State>
             style={{
               zIndex: i === this.state.activeTab ? tabCount : tabCount - i - 1
             }}
-            onClick={() => this.onTabClick( i )}
+            onMouseDown={() => this.onMouseDown( i )}
+            draggable={true}
+            onDragStart={( e ) => this.onDragStart( i, e )}
+            onDragEnter={( e ) => this.onDragEnter( i, e )}
+            onDragOver={( e ) => this.onDragOver( i, e )}
+            onDragEnd={( e ) => this.onDragEnd( i, e )}
           >
             {tab}
           </li>
@@ -46,9 +53,48 @@ class DocumentTabs extends React.Component<{}, State>
     );
   }
 
-  private onTabClick = ( tabIndex: number ) =>
+  private onMouseDown = ( tabIndex: number ) =>
   {
     this.setState( { activeTab: tabIndex } );
+  }
+
+  private onDragStart = ( tabIndex: number, e: React.DragEvent<HTMLLIElement> ) =>
+  {
+    this.setState( { dragging: tabIndex } );
+  }
+
+  private onDragEnter = ( tabIndex: number, e: React.DragEvent<HTMLLIElement> ) =>
+  {
+    this.dragTab( tabIndex );
+  }
+
+  private onDragOver = ( tabIndex: number, e: React.DragEvent<HTMLLIElement> ) =>
+  {
+    e.preventDefault();
+  }
+
+  private onDragEnd = ( tabIndex: number, e: React.DragEvent<HTMLLIElement> ) =>
+  {
+    this.setState( { dragging: null } );
+  }
+
+  private dragTab( tabIndex: number )
+  {
+    this.setState( ( prevState ) =>
+    {
+      if( prevState.dragging !== null && prevState.dragging !== tabIndex )
+      {
+        let newTabs = [ ...prevState.tabs ];
+        let a = newTabs[ prevState.dragging ];
+        newTabs[ prevState.dragging ] = newTabs[ tabIndex ];
+        newTabs[ tabIndex ] = a;
+        return { tabs: newTabs, dragging: tabIndex, activeTab: tabIndex };
+      }
+      else
+      {
+        return { ...prevState };
+      }
+    } );
   }
 }
 
